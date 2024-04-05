0a771ad551978a1d4e80cf75a9cf23712d28dead
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-04-04 20:42:04 -0400

    more work on card

--- src/pages/Card/Card.jsx
+++ src/pages/Card/Card.jsx
@@ -1,11 +1,18 @@
 import "./Card.scss";
 import HeatLogo from "../../assets/logos/Heat.svg";
-import BamAdebayo from "../../assets/PlayerPics/BamAdebayo.png"
+import BamAdebayo from "../../assets/PlayerPics/BamAdebayo.png";
 
 const Card = () => {
   return (
     <main className="teamMain">
-      <img src={BamAdebayo} alt="" className="teamMain__pic" />
+      {/* <img src={BamAdebayo} alt="" className="teamMain__pic" /> */}
+      <section className="teamMain__infoDiv">
+        <p className="teamMain__name">BAM</p>
+        <p className="teamMain__lastName">ADEBAYO</p>
+        <p className="teamMain__position">CENTER-FORWARD</p>
+        <button className="teamMain__button">+ STATS
+        </button>
+      </section>
     </main>
   );
 };
