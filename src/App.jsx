7f8182c34d0732041e1dd8ad42b35b40fe034a4a
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-04-05 13:55:50 -0400

    patch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./App.scss";
 import Data from "./assets/Data/NBAData.json";
@@ -9,10 +9,13 @@ import Team from "./pages/Team/Team";
 function App() {
   const [team, setTeam] = useState(Data[0]);
 
+  const navigate = useNavigate()
+
   return (
     <BrowserRouter>
       <div className="topBar"></div>
       <Routes>
+        <Route path="/" element={<Navigate to={"/:team:playerName"} />} />
         <Route path="/:team/:playerName" element={<Card player={team.players[2]} />} />
         <Route path="/team/:teamName" element={<Team team={team} />} />
       </Routes>
